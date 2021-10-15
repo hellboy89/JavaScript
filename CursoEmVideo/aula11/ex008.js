1ff852c49e973fdd8d4ba8c85d4efa46a4e94ea4
Juan Cleber <juan.fsj@gmail.com>
2021-10-15 07:31:28 -0300

    aula condicionais

---
+++ CursoEmVideo/aula11/ex008.js
@@ -0,0 +1,19 @@
+var vel = 200.5
+console.log(`a velocidade do seu carro é ${vel}km/h.`)
+console.log(`dirija sempre usando cinto de segurança!`)
+
+if ( vel >= 100 ) {
+    console.log(`está acima do limite reduza`)
+}
+else {
+    console.log(`continue assim.`)
+}
+
+var pais = 'Brasil'
+
+if (pais == 'Brasil') {
+    console.log('Brasileiro')
+}
+else {
+    console.log('Estrangeiro')
+}
---
+++ CursoEmVideo/aula12/ex011.js
@@ -0,0 +1,14 @@
+var idade = 45
+
+console.log(`Você tem ${idade} anos. Então...`)
+
+if (idade < 16) {
+    console.log('Não vota')
+}
+else if (idade < 18) {
+    console.log('Voto Opcional')
+}
+else {
+    console.log('Voto obrigatório')
+}
+
